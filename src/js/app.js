9dd1551df24d2c8a2f50935bba94d30f4af5a402
Adrian Guzman Lopez <adrienguzmanlopez@gmail.com>
2022-03-23 23:38:15 -0600

    Dashboard responsive  v1

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,55 @@
 /**
  *
- *
+ * CONST
  */
-
 const sideMenu = document.querySelector("aside"),
    menuBtn = document.querySelector("#menu-btn"),
-   closeBtn = document.querySelector("#close-btn");
+   closeBtn = document.querySelector("#close-btn"),
+   themeToggler = document.querySelector(".theme-toggler");
 
+/**Show sidebar */
 menuBtn.addEventListener("click", () => {
    sideMenu.style.display = "block";
 });
 
+/**Close sidebar */
 closeBtn.addEventListener("click", () => {
    sideMenu.style.display = "none";
+});
+
+// Change Theme Dashboard
+themeToggler.addEventListener("click", () => {
+   document.body.classList.toggle("dark-theme-variables");
+
+   // themeToggler.querySelector("span:nth-child(1)").classList.toggle("active");
+   // themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+
+   themeToggler.querySelector("span").classList.toggle("active");
+   themeToggler.style.color = "#ffbb55";
+   themeToggler.querySelector("span:nth-child(2)").classList.toggle("active");
+   themeToggler.querySelector("span:nth-child(2)").style.color = "#000";
+});
+
+/**
+ * FILL ORDERS IN TABLE
+ */
+
+Orders.forEach((order) => {
+   const tr = document.createElement("tr"),
+      trContent = `
+                     <td>${order.productName}</td>
+                     <td>${order.productNumber}</td>
+                     <td>${order.paymentStatus}</td>
+                     <td class="${
+                        order.shipping === "Declined"
+                           ? "danger"
+                           : order.shipping === "Pending"
+                           ? "warning"
+                           : "primary"
+                     }">${order.shipping}</td>
+                     <td class="primary">Details</td>
+                  `;
+
+   tr.innerHTML = trContent;
+   document.querySelector("table tbody").appendChild(tr);
 });
---
+++ src/js/orders.js
@@ -0,0 +1,49 @@
+/**
+ *
+ * Orders
+ */
+
+const Orders = [
+   {
+      productName: "Foldable Mini Drone",
+      productNumber: "85236",
+      paymentStatus: "Due",
+      shipping: "Pending",
+   },
+   {
+      productName: "LARVENDER",
+      productNumber: "25687",
+      paymentStatus: "Refunded",
+      shipping: "Declined",
+   },
+   {
+      productName: "Ruko F11 Pro Drone",
+      productNumber: "25997",
+      paymentStatus: "Due",
+      shipping: "Pending",
+   },
+   {
+      productName: "Drone with Camera Drone",
+      productNumber: "96572",
+      paymentStatus: "Paid",
+      shipping: "Delivered",
+   },
+   {
+      productName: "GPS 4K Drone",
+      productNumber: "21245",
+      paymentStatus: "Paid",
+      shipping: "Delivered",
+   },
+   {
+      productName: "DJI Air 2S",
+      productNumber: "24589",
+      paymentStatus: "Due",
+      shipping: "Pending",
+   },
+   {
+      productName: "Lozenge Drone",
+      productNumber: "21247",
+      paymentStatus: "Paid",
+      shipping: "Delivered",
+   },
+];
